fceb6e1181fdfa5ff059214fba7d02914f1faf22
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-02-26 19:54:45 +0000

    finalizo henerador excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let newExcuse = () => {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let whoNew = Math.floor(Math.random() * who.length);
+    let actionNew = Math.floor(Math.random() * action.length);
+    let whatNew = Math.floor(Math.random() * what.length);
+    let whenNew = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoNew] +
+      " " +
+      action[actionNew] +
+      " " +
+      what[whatNew] +
+      " " +
+      when[whenNew]
+    );
+  };
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = newExcuse();
+  });
 };
